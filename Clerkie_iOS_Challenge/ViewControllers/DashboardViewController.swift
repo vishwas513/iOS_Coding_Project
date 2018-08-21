73405366f114be9104518d6c022d37d6f4cbdd6b
Vishwas Mukund <vishwas.mukund@sjsu.edu>
2018-08-21 13:50:49 -0700

    Organization, Basic UI layout, networking class completed

---
+++ Clerkie_iOS_Challenge/ViewControllers/DashboardViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DashboardViewController.swift
+//  Clerkie_iOS_Challenge
+//
+//  Created by Vishwas Mukund on 8/21/18.
+//  Copyright © 2018 Vishwas Mukund. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DashboardViewController : UIViewController {
+    
+    @IBOutlet weak var segment: UISegmentedControl!
+    @IBAction func segment(_ sender: Any) {
+        let selection = segment.selectedSegmentIndex;
+        if(selection == 0){
+           // segment.selectedSegmentIndex = 0;
+            let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil);
+            let ChatViewController = storyBoard.instantiateViewController(withIdentifier: "chatViewController") as! UIViewController
+            self.present(ChatViewController , animated:true, completion:nil)
+            
+        }else if(selection == 1){
+            let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil);
+            let ChatViewController = storyBoard.instantiateViewController(withIdentifier: "dashboardViewController") as! UIViewController
+            self.present(ChatViewController , animated:true, completion:nil)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        segment.selectedSegmentIndex = 1;
+        
+    }
+
+    
+}
