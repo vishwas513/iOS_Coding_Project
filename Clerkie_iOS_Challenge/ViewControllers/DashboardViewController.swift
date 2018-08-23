d6e75068329e66ed6ade162c82fb65968c97efbb
Vishwas Mukund <vishwas.mukund@sjsu.edu>
2018-08-22 21:00:07 -0700

    Vertical and Horizontal Bar Charts completed

--- Clerkie_iOS_Challenge/ViewControllers/DashboardViewController.swift
+++ Clerkie_iOS_Challenge/ViewControllers/DashboardViewController.swift
@@ -12,6 +12,9 @@ import Charts
 
 class DashboardViewController : UIViewController {
     
+    @IBOutlet weak var barChart: BarChartView!
+    @IBOutlet weak var horizontalBarChart: HorizontalBarChartView!
+    @IBOutlet weak var lineChart: LineChartView!
     @IBOutlet weak var segment: UISegmentedControl!
     @IBAction func segment(_ sender: Any) {
         let selection = segment.selectedSegmentIndex;
@@ -32,6 +35,24 @@ class DashboardViewController : UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         segment.selectedSegmentIndex = 1;
+        let entry1 = BarChartDataEntry(x: 1.0, y: Double(10))
+        let entry2 = BarChartDataEntry(x: 2.0, y: Double(50))
+        let entry3 = BarChartDataEntry(x: 3.0, y: Double(100))
+        
+        
+        
+        let dataSet = BarChartDataSet(values: [entry1, entry2, entry3], label: "Widgets Type")
+        let data = BarChartData(dataSets: [dataSet])
+        barChart.data = data
+        barChart.chartDescription?.text = "Number of Widgets by Type"
+        
+        horizontalBarChart.data = data;
+        horizontalBarChart.chartDescription?.text = "Number of Widgests"
+        //All other additions to this function will go here
+        
+        //This must stay at end of function
+        barChart.notifyDataSetChanged()
+        horizontalBarChart.notifyDataSetChanged()
         
     }
 
